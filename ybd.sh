47b75c4d90d0b508e8bb3d06f23f35bf392fc951
sudobyter-hub <sudobyter@gmail.com>
2021-08-20 18:31:11 +0300

    ready to use to download youtube mp3

--- ybd.sh
+++ ybd.sh
@@ -1,9 +1,10 @@
 !# /bin/bash 
 
+
 echo "Please write the URL"
 
 #reading the url 
-read $url 
+read url 
 
 # using youtube-dl to download 
 youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $url 
